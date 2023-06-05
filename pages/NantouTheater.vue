b0fce902e74e0702926df10463a7a201091a483f
cookie_opjax <vigor01695@gmail.com>
2023-06-05 16:10:00 +0800

    feat : add Nantou Theater page

---
+++ pages/NantouTheater.vue
@@ -0,0 +1,78 @@
+<template>
+  <main class="tw-flex tw-items-center tw-justify-center tw-flex-col tw-p-0">
+    <div class="tw-w-full tw-px-64 tw-py-10">
+      <h1 class="tw-text-titleGreen tw-text-7xl tw-w-full tw-mb-8">南投戲院</h1>
+      <div class="tw-flex tw-gap-8">
+        <div class="tw-w-96">
+          <h4 class="tw-font-bold">百年老戲院挺過低谷</h4>
+          <h4 class="tw-font-bold">用人情味留存在地人記憶</h4>
+          <hr />
+          <p class="tw-text-end tw-mb-3">文/白欣卉</p>
+          <p>
+            「南投戲院」為南投市僅存的戲院，也是目前全台灣首輪電影院中歷史最悠久的一家老戲院。南投戲院的前身是「悅舞台」，成立於1920年，一直到民國40年代，由第一代蔡瑞炎買下其他九個股東的股份，才正式更名為南投戲院。然而經歷921地震後，南投地區經濟受到重創，大環境的改變使戲院一度面臨轉賣，原在台北公關公司上班的第三代經營人蔡杰峰因不捨戲院消失，決定放下一切回到故鄉，本以為留住老戲院之後就能再去做自己想做的事，可到後來卻發現戲院與在地的連結羈絆變得無法切割，才知道自己已經離不開了。
+          </p>
+        </div>
+        <div class="tw-h-full tw-w-full">
+          <img src="../assets/natou_cons.jpg" alt="" class="tw-h-full tw-w-auto" />
+          <p class="tw-text-end">南投戲院建築外貌。攝/白欣卉</p>
+        </div>
+      </div>
+    </div>
+    <div class="tw-w-full tw-px-64 tw-bg-[#92ad91] tw-text-white tw-py-10">
+      <h2 class="tw-text-5xl tw-mb-2 tw-text-center tw-font-bold">青年返鄉注入新力</h2>
+      <h2 class="tw-text-5xl tw-mb-8 tw-text-center tw-font-bold">翻轉戲院</h2>
+      <div class="tw-flex tw-gap-8">
+        <div class="tw-w-1/2">
+          <p class="tw-mb-4">
+            蔡杰峰從台北回鄉後，第一個構想的就是如何吸引更多觀眾走進戲院。大學就讀餐旅系且學生時代曾拜師學習烘焙與沖煮咖啡技術，於是有了經營咖啡廳這個想法。蔡杰峰認為咖啡店適合跟文創及復古懷舊元素去做結合，於是十年前毅然決然把戲院旁邊的車庫改裝成了「老戲院café騎士團」，當時是希望有一個說故事的平台，讓戲院有實體的管道與民眾連結。
+          </p>
+          <p>
+            直到2021年戲院做了一個大轉型，將現在的咖啡廳與戲院打通，轉變成一個售票及戲院的餐飲空間，做出一條龍動線，並集結地方力量聯合一些在地設計品牌，推出各種文創商品，以此吸引更多族群。
+          </p>
+        </div>
+        <div class="tw-w-1/2">
+          <p class="tw-mb-4">
+            近年來南投戲院常結合電影來舉辦各式不同活動，例如結合《灌籃高手The First Slam
+            Dunk》舉辦跨年特別場活動，不僅可以在戲院內用放映設備一起打「灌籃高手」電動，還能搶先觀賞到電影特映。又或者是2021年大賣座的國片電影《月老》，戲院特別準備了專屬的緣結卡，可以寫下喜愛對象的姓名掛在戲院的姻緣牆上，之後戲院會將所有緣結卡一起帶到日月潭的龍鳳宮向月老祈願，這樣與地方公廟宗教文化去結合的活動，在行銷電影的同時也是在行銷地方。
+          </p>
+          <p>
+            蔡杰峰結合公關業界所學，將電影當成一個新目標，透過不同的電影去進行各種企劃，只為了讓大家有動力想要來到南投。蔡杰峰表示：「讓更多人看到戲院，同樣就會讓更多人看到南投市，但讓更多人看到南投市，也相對會讓更多人看到戲院。在沒有電影的時候我們就推廣南投市，有電影的時候就換推廣戲院了。」
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="tw-w-full tw-px-64 tw-bg-[#515e5a] tw-text-white tw-py-10">
+      <div class="tw-flex tw-gap-8">
+        <div class="tw-w-1/2 tw-flex-shrink-0">
+          <h4 class="tw-font-bold tw-text-4xl">不和市區做比較</h4>
+          <h4 class="tw-font-bold tw-text-4xl">親切度才是老戲院王道</h4>
+          <hr class="my-6" />
+          <p>
+            想要跟上時代，硬體升級必定是趨勢，十年前南投戲院因虧損兩千多萬，沒有太多餘力可以去做硬體的更新，十年過去了，現在老戲院因為有了大家的支持漸漸累積到了資源，開始積極提升硬體設備反饋給大家。蔡杰峰說：「戲院的定義比較像是一個大家長期以來的好鄰居，這個鄰居的關係不遠也不近，大家認識它，雖然沒有像朋友，但也不疏離。」戲院總是能知道民眾想要的東西是什麼，也知道大家有什麼點不可以踩。老戲院不賣很貴的周邊，反而用盡辦法透過活動送給大家。蔡杰峰憑藉地方獨有的親切度，把電影跟在地去做連結，這樣的溫暖人情，讓老戲院能夠在這什麼都追求新潮的世代存活。
+          </p>
+        </div>
+        <div class="tw-h-full tw-w-full">
+          <v-carousel>
+            <v-carousel-item :src="carousel1" cover></v-carousel-item>
+            <v-carousel-item :src="carousel2" cover></v-carousel-item>
+            <v-carousel-item :src="carousel3" cover></v-carousel-item>
+            <v-carousel-item :src="carousel4" cover></v-carousel-item>
+            <v-carousel-item :src="carousel5" cover></v-carousel-item>
+            <v-carousel-item :src="carousel6" cover></v-carousel-item>
+            <v-carousel-item :src="carousel7" cover></v-carousel-item>
+          </v-carousel>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+import carousel1 from "../assets/theater_carousel/1.jpg";
+import carousel2 from "../assets/theater_carousel/2.jpg";
+import carousel3 from "../assets/theater_carousel/3.jpg";
+import carousel4 from "../assets/theater_carousel/4.jpg";
+import carousel5 from "../assets/theater_carousel/5.jpg";
+import carousel6 from "../assets/theater_carousel/6.jpg";
+import carousel7 from "../assets/theater_carousel/7.jpg";
+</script>
+<style scoped></style>

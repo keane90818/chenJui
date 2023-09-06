8cd6e19b6e99e692de17a1992ad4097e95786fef
Hua <a47894785@gmail.com>
2023-09-06 15:52:35 +0800

    0906 - tranditional crafts page almost done (non-responsive)

--- pages/traditionalCrafts/index.vue
+++ pages/traditionalCrafts/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div id="container" class="tw-w-full tw-flex-col tw-flex tw-items-center">
+  <div class="tw-h-screen tw-flex tw-flex-col tw-mx-auto tw-my-auto">
+    <div id="container" class="tw-w-full tw-flex-col tw-flex tw-items-center tw-mx-auto tw-my-auto">
       <div class="info-container tw-flex tw-flex-row tw-my-16">
         <div class="text-container tw-pt-[20px]">
           <div class="title tw-text-7xl tw-font-semibold tw-mr-48 tw-text-[#c47f42]">傳統技藝</div>
@@ -13,26 +13,29 @@
           <img src="../../assets/map/traditional-crafts-map.jpg" alt="traditional-crafts-map" height="436" width="624">
         </div>
       </div>
-      <div class="detail-container tw-flex tw-flex-row">
+      <v-card id="detail-container" class="tw-mb-10">
         <div class="LaiFa-area tw-flex tw-flex-row">
-          <div class="img-container">來發img</div>
-          <div class="info-container">
-            <div class="title">來發鐵店</div>
-            <div class="desc">來發desc</div>
-            <div class="navigate">Read More</div>
+          <img src="../../assets/imgs/LaiFaImg.png" alt="LaiFa" height="320" width="220" class="traditional-crafts-img tw-mr-4">
+          <div class="area-info-container tw-w-[220px] tw-px-9 tw-py-5">
+            <div class="title tw-text-lg tw-font-semibold tw-text-[#000763]">來發鐵店</div>
+            <div class="desc tw-h-[280px] tw-break-all tw-text-sm tw-my-6 tw-leading-loose tw-text-[#4F5168]">當地第一間從清朝末年開始發跡的百年鐵舖「來發鐵店」，至今仍不改迷人的打鐵風景，堅持手工打造的鑄鐵技術遠近馳名，在現代更顯得特殊而珍貴，也讓人喜愛上手工鐵具獨有的溫度。</div>
+            <NuxtLink to="/traditionalCrafts" class="navigate tw-text-sm tw-font-semibold tw-text-[#FF6734]">Read More &rarr;</NuxtLink>
           </div>
+
         </div>
         <div class="GuanDe-area tw-flex tw-flex-row">
-          <div class="img-container">冠德img</div>
-          <div class="info-container">
-            <div class="title">冠德竹木精品社</div>
-            <div class="desc">冠德desc</div>
-            <div class="navigate">Read More</div>
+          <img src="../../assets/imgs/GuanDeImg.png" alt="GuanDe" height="320" width="220" class="traditional-crafts-img tw-mr-4">
+          <div class="area-info-container tw-w-[220px] tw-px-9 tw-py-5">
+            <div class="title tw-text-lg tw-font-semibold tw-text-[#000763]">冠德竹木精品社</div>
+            <div class="desc tw-h-[280px] tw-break-all tw-text-sm tw-my-6 tw-leading-loose tw-text-[#4F5168]">劉興澤自幼家境清寒，從國中畢業就因父親於竹木加工廠工作，開始接觸傳統竹工藝，在一次機緣下，開始跟著國寶級竹編大師黃塗山學習竹藝，成功成為黃塗山的嫡傳三大弟子之一，更獲得台灣工藝之家的評選認證。</div>
+            <NuxtLink to="/traditionalCrafts" class="navigate tw-text-sm tw-font-semibold tw-text-[#FF6734]">Read More &rarr;</NuxtLink>
           </div>
         </div>
-      </div>
+      </v-card>
     </div>
-    <div class="footer">&copy; Copyright 2022 POUND達. All Rights Reserved.</div>
+    <div class="footer-container tw-pl-[400px] tw-bg-[#857A70]">
+      <div class="footer tw-py-4 tw-text-[#f1ede2] tw-text-base">&copy; Copyright 2022 POUND達. All Rights Reserved.</div>
+    </div>
   </div>
 </template>
 
@@ -53,8 +56,22 @@
 }
 
 #container {
-  height: 1024px;
+  height: auto;
   width: 1905px;
+}
+
+#detail-container {
+  display: flex;
+  padding: 20px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.traditional-crafts-img {
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.area-info-container {
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 }
 
 </style>

e6badb97604dbece94d61b50d98936dfd4d912be
Hua <a47894785@gmail.com>
2024-02-20 17:20:56 +0800

    0220 - traditionalcrafts/LaiFa done

---
+++ pages/traditionalCrafts/LaiFa/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <main class="tw-flex tw-items-center tw-justify-center tw-flex-col tw-p-0">
+    <div class="tw-w-full tw-px-96 tw-pt-10">
+      <h1 class="tw-text-[#681535] tw-text-7xl tw-w-full tw-mb-8">來發鐵店</h1>
+      <div class="tw-flex tw-gap-20">
+        <div class="tw-w-72">
+          <h4 class="tw-font-bold tw-text-[#7f6c8f]">竹山打鐵巷沒落</h4>
+          <h4 class="tw-font-bold tw-text-[#7f6c8f]">打鐵職人靠手藝迎擊</h4>
+          <hr class="tw-border-gray-500 tw-my-1" />
+          <p class="tw-text-end tw-mb-3 tw-text-gray-500">文/白欣卉</p>
+          <p class="tw-text-[18px] tw-font-medium tw-tracking-wide tw-text-gray-500 tw-leading-[1.5em]">
+            南投竹山因盛產竹子，早期農業社會需上山砍竹、挖竹筍，便開始有各式刀具的需求，於是在當地形成一條打鐵巷，其中的砍刀、割筍刀，或是耕作農田所需的鐮刀、鋤頭等，都由打鐵鋪所造。隨著產業變遷，自動化機械生產普及，傳統的打鐵店逐漸凋零，許多鐵舖也因為沒有後代繼承漸漸消失，不過當地第一間從清朝末年開始發跡的百年鐵舖「來發鐵店」，至今仍不改迷人的打鐵風景，堅持手工打造的鑄鐵技術遠近馳名，在現代更顯得特殊而珍貴，也讓人喜愛上手工鐵具獨有的溫度。
+          </p>
+        </div>
+        <div class="tw-h-[750px] tw-w-[750px]">
+          <img src="../../../assets/traditionalCrafts/LaiFa/1.jpg"  alt=""/>
+          <p class="tw-text-end tw-text-gray-500 tw-pt-2">郭慶順手工製刀過程火光飛濺。攝/白欣卉</p>
+        </div>
+      </div>
+    </div>
+    <div class="tw-w-full tw-px-96 tw-bg-[#E5E5E5] tw-text-[#535963] tw-py-40">
+      <div class="tw-flex tw-gap-20">
+        <div class="tw-w-[31rem]">
+          <h1 class="tw-font-bold tw-text-4xl ">星星之火的淬煉</h1>
+          <h1 class="tw-font-bold tw-text-4xl ">打鐵匠人的獨門技藝</h1>
+          <p class="tw-text-[18px] tw-font-medium tw-tracking-wide tw-leading-[1.5em] tw-pt-10">
+            來發鐵店第四代傳人郭慶順，最引以為傲自家菜刀是全台少數遵循古法「鐵包鋼」工藝製作。鐵包鋼的優點是好磨、有韌性，以及提升打鐵上的手感，祖傳鐵包鋼工藝就像包三明治一樣，將鋼條放到鐵塊裡，接著上下打鐵鍛造塑形，目的是為了將鐵與鋼緊密融合，歷經千錘百鍊，才能慢慢變成刀的雛形。儘管比起一般鋼板，鐵包鋼工序複雜，還得花上三、四個鐘頭反覆熔融鍛造，但刀具使用起來卻最堅固耐磨。
+          </p>
+        </div>
+        <div class="tw-h-[400px] tw-w-[700px]">
+          <v-carousel hide-delimiters height="400">
+            <v-carousel-item :src="carousel1" cover width="100%" height="400px"></v-carousel-item>
+            <v-carousel-item :src="carousel2" cover width="100%" height="400px"></v-carousel-item>
+            <v-carousel-item :src="carousel3" cover width="100%" height="400px"></v-carousel-item>
+          </v-carousel>
+        </div>
+      </div>
+    </div>
+    <div class="tw-w-full tw-px-96 tw-bg-[#7e7e7e] tw-text-white tw-pt-20">
+      <div class="tw-flex tw-gap-10 tw-h-full">
+        <div class="tw-h-[500px] tw-w-[480px]">
+          <img src="../../../assets/traditionalCrafts/LaiFa/3.jpg"  alt=""/>
+        </div>
+        <div class="tw-w-[33rem]">
+          <h1 class="tw-font-bold tw-text-4xl ">百年傳承</h1>
+          <h1 class="tw-font-bold tw-text-4xl ">剛硬鐵具後的鐵漢柔情</h1>
+          <hr class="tw-my-4" />
+          <p class="tw-text-[18px] tw-font-medium tw-tracking-wide tw-leading-[1.5em]">
+            郭慶順從十四歲開始就跟在父親身旁學打鐵，在現今壓模就可以輕鬆做一把刀的時代，他還堅持著手工鍛敲及客製化的服務，憑藉的就是他五十多年來累積的手藝與經驗。郭慶順強調慢工才能出細活，因為講求品質所以沒辦法追求快速，為了保持傳統技藝的功夫，任何一道製作程序都不能馬虎。含著「鐵湯匙」出生的郭慶順帶著鋼鐵般的意志打了一輩子的鐵，心中有著傳統技藝傳承的夢想，目前與第五代兒子郭志榮父子倆共同經營。郭志榮原本擔任電子工程師，儘管前景優渥，但爸爸郭慶順卻說：「工程師很多，打鐵師很少。」因為這句話，郭志榮決定放下一切回鄉當起打鐵師傅。
+          </p>
+        </div>
+      </div>
+      <div class="tw-flex tw-gap-10 tw-h-full">
+        <div class="tw-h-[500px] tw-w-[480px]">
+          <img src="../../../assets/traditionalCrafts/LaiFa/4.jpg"  alt=""/>
+          <p class="tw-mt-2">百年傳承，手工製刀。攝/白欣卉</p>
+        </div>
+        <div class="tw-w-[33rem]">
+          <h1 class="tw-font-bold tw-text-4xl ">父子攜手拚轉型</h1>
+          <h1 class="tw-font-bold tw-text-4xl ">為夕陽產業走出新路</h1>
+          <hr class="tw-my-4" />
+          <p class="tw-text-[18px] tw-font-medium tw-tracking-wide tw-leading-[1.5em]">
+            以前是農業社會，山上的竹子要用柴刀砍，開路也要用鋤頭，現在砍竹子都是使用電器除草機或挖土機，隨著農業時代的結束，許多傳統打鐵店一家家關門，郭慶順和郭志榮父子倆則一起攜手轉型，推出藝術刀及客製化製刀服務，像是閃耀著美麗水波紋的武士刀、開山刀、片刀，又或是可隨身攜帶的鑰匙圈刀具都能夠訂製，純手工鍛造所形成的迷人紋路，讓堅守百年打鐵工藝的郭家父子重新找出生存之道。
+          </p>
+          <p class="tw-text-[18px] tw-font-medium tw-tracking-wide tw-leading-[1.5em] tw-mt-5">
+            也許打造傳統農具已經式微，但純手工客製的刀具仍受到不少民眾喜愛，每一把用汗水與心意打造的刀，都存在著匠人的溫度與其中的歷史記憶。
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="tw-w-full tw-px-96 tw-bg-[#E5E5E5] tw-text-[#535963] tw-py-20">
+      <div class="tw-flex tw-gap-20">
+        <iframe width="1259" height="708" src="https://www.youtube.com/embed/4rp1iBNHqUQ" title="陳酒家釀-來發鐵店" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+import carousel1 from "../../../assets/traditionalCrafts/LaiFa/2_1.jpg";
+import carousel2 from "../../../assets/traditionalCrafts/LaiFa/2_2.jpg";
+import carousel3 from "../../../assets/traditionalCrafts/LaiFa/2_3.jpg";
+</script>
+<style scoped></style>

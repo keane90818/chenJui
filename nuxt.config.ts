fd23bd9dc144f6c2996de75856bca53f64ccb6d4
keane Chien <keane90818@gmail.com>
2025-01-10 11:15:40 +0800

    新增 Google Analytics 4 腳本和 Nuxt 配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
-  ssr:true,
-  css: ['~/assets/css/main.css',"vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.min.css","animate.css/animate.min.css"],
+  ssr: true,
+  css: [
+    '~/assets/css/main.css',
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+    "animate.css/animate.min.css"
+  ],
   typescript: {
     typeCheck: true
   },
   build: {
     transpile: ["vuetify"]
   },
-  
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -25,8 +27,33 @@ export default defineNuxtConfig({
   },
   modules: [
     [
-      "unplugin-icons/nuxt",{}
+      "unplugin-icons/nuxt", {}
     ],
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  plugins: [
+    '~/plugins/google-analytics.client.ts', // 新增的插件路徑
+  ],
+  app: {
+    head: {
+      script: [
+        {
+          hid: 'google-tag',
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-V3VTFF8PLS',
+        },
+        {
+          hid: 'google-analytics',
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V3VTFF8PLS');
+          `,
+          type: 'text/javascript',
+        },
+      ],
+      __dangerouslyDisableSanitizers: ['script'], // 使用正確的屬性
+    },
+  },
 });
